23ec381f3c55efede570c0cc98fb8402d94d546f
JustinSHong <jhong2009@gmail.com>
2018-08-04 11:11:42 -0700

    set up imports and data_file variable

---
+++ main.py
@@ -0,0 +1,8 @@
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.pagesize import landscape
+from reportlab.platypus import Image
+
+import csv
+
+data_file = 'data.csv'
